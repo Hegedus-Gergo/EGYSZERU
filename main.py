32404b0dd61f63b3c44a7663e99848f9e003357c
gazdi435 <72067999+gazdi435@users.noreply.github.com>
2022-09-08 12:55:27 +0200

    yes

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+
+veletlen_szamok_listaja = []
+
+for veletlen_szam in range(6):
+    veletlen_szam = random.randint(1,15)
+    veletlen_szamok_listaja.append(veletlen_szam)
+
+print(veletlen_szamok_listaja)
+
+for sorszam in range(0, len(veletlen_szamok_listaja)):
+    for ertek in range(sorszam + 1, len(veletlen_szamok_listaja)):
+        if veletlen_szamok_listaja[sorszam] > veletlen_szamok_listaja[ertek]:
+            veletlen_szamok_listaja.insert(sorszam, veletlen_szamok_listaja[ertek])
+            veletlen_szamok_listaja.pop(ertek + 1)
+
+print(veletlen_szamok_listaja)
